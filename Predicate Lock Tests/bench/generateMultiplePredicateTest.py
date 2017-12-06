575f746fa0d0919837f0cac130e1b168a5770a69
Robert Claus <robertclaus@gmail.com>
2017-12-06 14:34:38 -0600

    fix test changes

--- Predicate Lock Tests/bench/generateMultiplePredicateTest.py
+++ Predicate Lock Tests/bench/generateMultiplePredicateTest.py
@@ -1,18 +1,26 @@
 from random import randint
 
 numberOfPredicates= [1,10,100,1000,10000]
-numberOfQueries = 100
+numberOfQueries = 10
+setCounter=1;
+whereCounter=1000000;
 
 for preds in numberOfPredicates:
 	f = open('varyingPredicateCountQueries'+str(preds)+'.txt','w')
-	query='UPDATE Child SET a=1'
-	if preds>1:
-		query=query+' WHERE '
-		for i in range(1, preds):
-			query=query+'a='+str(i)+' '
-			if not i==preds-1:
-				query=query+'OR '
 	for i in range(1,numberOfQueries):
+		query='UPDATE Child SET a='+str(setCounter)
+		setCounter=setCounter+1;
+		wherequery=''
+		if preds>0:
+			wherequery=wherequery+' WHERE '
+			for i in range(0, preds):
+				wherequery=wherequery+'a='+str(whereCounter)+' '
+				whereCounter=whereCounter+1
+				if not i==preds-1:
+					wherequery=wherequery+'OR '
+		query=query+wherequery
 		f.write(query+';\n')
 	f.write('\dt')
+	f.write('\n')
+	f.write('\n')
 	f.close()
